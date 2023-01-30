d159138e4c4545afb975c07bbacdcea24f2e3797
Gregak-7 <92618854+Gregak-7@users.noreply.github.com>
2023-01-30 10:37:46 -0600

    Add files via upload

---
+++ Module 2/Book Assignments/4dot2.py
@@ -0,0 +1,11 @@
+small = False
+green = False
+
+if small == True and green == True:
+    print("Cherry")
+elif small == True and green == False:
+    print("Pea")
+elif small == False and green == True:
+    print("Watermelon")
+else:
+    print("Pumpkin")
