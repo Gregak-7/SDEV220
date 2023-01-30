278d5d761a8c6046c5a9d011bc3281244eed6c5d
Gregak-7 <92618854+Gregak-7@users.noreply.github.com>
2023-01-30 10:55:24 -0600

    Update 6dot2.py

--- Module 2/Book Assignments/Chapter 6/6dot2.py
+++ Module 2/Book Assignments/Chapter 6/6dot2.py
@@ -1,3 +1,9 @@
+#Name: Greg Kocal
+#Version: Python 3
+#Purpose of Life: Choose a number between 1 and 10 and assign it to the variable secret. Then, select another number 
+#between 1 and 10 and assign it to the variable guess. Next, write the conditional tests (if, else, and elif) to print the string 
+#'too low' if guess is less than secret, 'too high' if greater than secret, and 'just right' if equal to secret.
+
 guess_me = 7
 number = 0
 
@@ -11,3 +17,8 @@ while number <= guess_me:
         print("oops")
         break
     number += 1
+    
+    
+    *******
+    #Bug Report:
+    #None to report
