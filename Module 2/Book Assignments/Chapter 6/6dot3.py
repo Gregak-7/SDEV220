532d1a6b75ce66d7fddc1bdb1782dd6c1db41969
Gregak-7 <92618854+Gregak-7@users.noreply.github.com>
2023-01-30 10:58:00 -0600

    Update 6dot3.py

--- Module 2/Book Assignments/Chapter 6/6dot3.py
+++ Module 2/Book Assignments/Chapter 6/6dot3.py
@@ -1,3 +1,9 @@
+#Name: Greg Kocal
+#Version: Python 3
+#Purpose of Life: Assign the value 5 to the variable guess_me. Use a for loop to iterate a variable called number over range(10). 
+#If number is less than guess_me, print 'too low'. 
+#If it equals guess_me, print found it! and then break out of the for loop. If number is greater than guess_me, print 'oops' and then exit the loop.
+
 guess_me = 5
 
 for number in range(10):
@@ -9,3 +15,6 @@ for number in range(10):
     else:
         print('oops')
         break
+*******
+#Bug Report
+#No Bugs in the system... we live to fight another day
