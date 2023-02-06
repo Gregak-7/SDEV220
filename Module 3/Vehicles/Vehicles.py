5f20a8359443fc41e65f188693ecc67e887ff4bc
Gregak-7 <92618854+Gregak-7@users.noreply.github.com>
2023-02-06 09:06:17 -0600

    Add files via upload

---
+++ Module 3/Vehicles/Vehicles.py
@@ -0,0 +1,27 @@
+class Vehicle:
+    def __init__(self, vehicle_type):
+        self.vehicle_type = vehicle_type
+
+class Automobile(Vehicle):
+    def __init__(self, year, make, model, doors, roof, vehicle_type="car"):
+        super().__init__(vehicle_type)
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+
+car = Automobile(
+    year=input("Enter the year: "),
+    make=input("Enter the make: "),
+    model=input("Enter the model: "),
+    doors=input("Enter the number of doors: "),
+    roof=input("Enter the type of roof: "),
+)
+
+print("Vehicle type:", car.vehicle_type)
+print("Year:", car.year)
+print("Make:", car.make)
+print("Model:", car.model)
+print("Number of doors:", car.doors)
+print("Type of roof:", car.roof)
