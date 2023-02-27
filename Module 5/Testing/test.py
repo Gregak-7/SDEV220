ea62176d0e032787859e448e1f8940f596007d3c
Gregak-7 <92618854+Gregak-7@users.noreply.github.com>
2023-02-27 13:16:36 -0600

    Add files via upload

---
+++ Module 5/Testing/test.py
@@ -0,0 +1,30 @@
+from fractions import Fraction
+import unittest
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+    
+# I found this testing to be very intetesting and to be infomrational 
+#since it delves deeper into testing programs and utilizing the shell..
+#The results were interesting and I liked how it described why things 
+#failed.
